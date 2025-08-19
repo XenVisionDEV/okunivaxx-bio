b05851e689fa77335a1dc534a091ff072c199b62
XenVisionDEV <okunew_psd@mail.ru>
2025-08-19 16:19:29 +0800

    Update ThemeToggle.tsx

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Moon, Sun } from 'lucide-react'
-import { motion } from 'motion/react'
+import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 
 export function ThemeToggle() {
